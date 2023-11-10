cc94bee41036852b0224d1cc96f756004195934f
Robin Engler <robin.engler@gmail.com>
2023-11-10 17:02:06 +0100

    course 2: formating of solution 31

--- course2/solutions/solution_31_multiprocessing.py
+++ course2/solutions/solution_31_multiprocessing.py
@@ -4,17 +4,16 @@
 
 import multiprocessing as mp
 
-def get_data_parts(N,nb_processes):
+def get_data_parts(N, nb_processes):
     # Data is composed of as many sub-lists as there are processes.
     data = [ [] for i in range(nb_processes)]
 
     for i in range(N):    
-        data[ i%(nb_processes) ].append( i )
-        
-        ## Here, the trick is that we use the % operator.
-        ## For instance for 2 processes:
-        ## * If i is 3, then i%2 == 1 -> goes to sublist 1.
-        ## * If i is 4, then i%2 == 0 -> goes to sublist 0.
+        # Here, the trick is that we use the % operator.
+        # For instance for 2 processes:
+        # * If i is 3, then i % 2 == 1 -> goes to sublist 1.
+        # * If i is 4, then i % 2 == 0 -> goes to sublist 0.
+        data[i % nb_processes].append(i)
     return data
 
 # Let's test it out: say we have 10 points, and 2 processes.
@@ -24,29 +23,29 @@ data_parts = get_data_parts(N, nb_processes)
 print(data_parts)
 
 # Then the task is to apply `f2` to a set of points:
-def f2(i , a, dx):
+def f2(i, a, dx):
     x = a + i * dx
     return x ** 2 - x
 
-def task(data , a , dx):
+def task(data, a, dx):
     s = 0
     for d in data:
-        s += f2(d , a , dx )
-    return s*dx
+        s += f2(d, a, dx)
+    return s * dx
 
-#let's work outside the function and focus on the main loop:
+# Let's work outside the function and focus on the main loop:
 a = 0
 b = 2
-N = 100#2 * 10**7
+N = 100  #2 * 10**7
 dx = (b - a) / N
 
 data_parts = get_data_parts(N, 2)
 
 # Test it on our test data.
-S1 , S2 = task(data_parts[0],a,dx), task(data_parts[1],a,dx)
-print("data parts results:",S1 , S2)
-# of course at the end you want to sum
-print("final result:", S1+S2)
+S1, S2 = task(data_parts[0], a, dx), task(data_parts[1], a, dx)
+print("data parts results:", S1, S2)
+# of course at the end you want to sum:
+print("final result:", S1 + S2)
 
 # Now, to be applied with map, we will make a specific version of the task
 # with a and dx fixed
